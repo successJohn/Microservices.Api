d2499e1d33948bd98c5c61f1914f064318127b75
Success John <successojohn@gmail.com>
2022-08-23 19:31:55 +0100

    Automapper Config

---
+++ PlatformService/Profiles/PlatformProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformProfile : Profile
+    {
+        public PlatformProfile()
+        {
+            // Source to Target
+
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+}
