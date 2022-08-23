fe868989d2f53e788954b6e16adeececbeaffd5f
Success John <successojohn@gmail.com>
2022-08-23 19:30:42 +0100

    Initial Project Setup and initial seeding

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,31 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo: IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            _context.Platforms.Add(platform);
+        }
+        public Platform GetPlatformById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,51 @@
+﻿namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+
+        }
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Data");
+
+                context.Platforms.AddRange(
+                    new Models.Platform()
+                    {
+                        Name = "DotNet",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+
+                     new Models.Platform()
+                     {
+                         Name = "SQL Server",
+                         Publisher = "Microsoft",
+                         Cost = "Free"
+                     },
+
+                     new Models.Platform()
+                     {
+                         Name = "Kubernetes",
+                         Publisher = "Cloud Native Computing Foundation",
+                         Cost = "Free"
+                     }
+
+                    );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("We already have data");
+            }
+        }
+    }
+}
